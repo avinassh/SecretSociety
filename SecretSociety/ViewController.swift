a459ed185669d1e8a62591777264a2c6c8c972df
avi <avinashsajjan@gmail.com>
2015-03-02 12:46:44 +0530

    added code for toggle security.

--- SecretSociety/ViewController.swift
+++ SecretSociety/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    var secure: Bool = false { didSet { updateUI() } }
+    
+    private func updateUI() {
+        passwordField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+    }
+    
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
 
     override func didReceiveMemoryWarning() {
